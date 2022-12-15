3bd32d49c76f6fcac9c89ab8ac4c1a62f21e8cb1
abdelghafour-ettaqui <ettaqui88@gmail.com>
2022-12-15 16:47:06 +0100

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    roleId: [1, Validators.required],
+  });
+  isSubmitted = false;
+  roles = [
+    { id: 1, title: 'developer' },
+    { id: 2, title: 'qa' },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm.get('roleId')?.valueChanges.subscribe((roleId) => {
+      console.log('SEND API REQUEST AND UPDATE ROLE', roleId);
+    });
+  }
+
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.registerForm.value,
+      this.registerForm.invalid
+    );
+    this.isSubmitted = true;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
